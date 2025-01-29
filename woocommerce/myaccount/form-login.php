a449d9ffcaf07af693362ab4c6d2af0271094bab
Daria <dashaeke94@gmail.com>
2025-01-29 14:33:04 +0200

    v3

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -11,9 +11,9 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
+ * @see     https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 7.0.1
+ * @version 9.2.0
  */
 
 if (!defined('ABSPATH')) {
